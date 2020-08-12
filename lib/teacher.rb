1519248e64cddacb0bfa18920e9a3aafa4097e9a
Michael Rabon <nobar86@gmail.com>
2020-08-12 21:35:52 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,7 +8,7 @@ class Teacher < User
                "programming computers hacking learning terminal",
                "bash Ruby rvm update certs"]
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+ 
 
 
 end 
